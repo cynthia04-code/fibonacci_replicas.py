c1bb63781d2fa981e79b3d14115f494a4bcfeff7
cynthia04-code <cynthia04@ciencias.unam.mx>
2025-11-06 23:11:50 -0600

    Create fibonacci_haciaAdelanteGenerator1.py

---
+++ Sucesiones/fibonacci_haciaAdelanteGenerator1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Fibonacci con función Generator versión 2
+
+Calcula hacia adelante
+
+Tema: yield
+La sentencia yield suspende la ejecución de una función y devuelve un valor al invocador, pero conserva suficiente estado para que la 
+función pueda reanudarse donde la dejó. Cuando la función se reanuda, continúa la ejecución inmediatamente después de la última ejecución 
+de yield. Esto permite que su código genere una serie de valores a lo largo del tiempo, en lugar de calcularlos de una vez y devolverlos
+como una lista.
+      
+if __name__ == "__main__":
+    Sirve para ejecutar un bloque de código solo cuando el script se ejecuta directamente, y no cuando se importa 
+    como un módulo en otro archivo.
